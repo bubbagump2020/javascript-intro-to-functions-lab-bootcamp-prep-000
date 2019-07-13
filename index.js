b198cf3063b0a54d1c9da0ba6ca1d96ac871c7ee
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-13 23:11:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-var uppercase = "HELLO!"
-uppercase.toUpperCase() === uppercase
-
-var lowercase = "hello!"
-lowercase.toLowerCase() === lowercase
-
-var love = "I love you, Grandma."
 
 function shout(String){
   String = String.toUpperCase()
@@ -26,20 +19,5 @@ function logWhisper(String){
 }
 
 function sayHiToGrandma(String){
-  if(String === 'HELLO'){
-    return console.log("YES INDEED!")
-  } else if(String === 'hello'){
-    return console.log("I can't hear you!")
-  }
-  
-  if(String === love){
-    return console.log("I love you, too.")
-  }
   
 }
-
-sayHiToGrandma(love)
-sayHiToGrandma(lowercase)
-sayHiToGrandma(uppercase)
-sayHiToGrandma(lowercase)
-sayHiToGrandma(love)
